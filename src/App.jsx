9ad4ba48a0319c5eda441bd1f1cb0e74b4e9545c
Yahya Gilany <yahya.gilany@live.com>
2024-02-27 13:52:10 -0500

    stuff

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
+  
   return (
     <>
       <div>
           <img src={reactLogo} className="logo react" alt="React logo" />
       </div>
-      <h1>IT3049C</h1>
+      <h1>IT3049C-Spring2024</h1>
       <div className="card">
         <p>
           Edit <code>src/App.jsx</code> and save to update this view.
